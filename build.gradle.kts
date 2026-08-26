7f24c2adda49f62ddc0ee097532075b1c4850cd0
nabil-Tounarti <nabil.tounarti0@gmail.com>
2026-08-27 01:20:43 +0200

    feat: add liquibase for database integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
     // --- Application Starters ---
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    // implementation("org.springframework.boot:spring-boot-starter-liquibase")
+    implementation("org.springframework.boot:spring-boot-starter-liquibase")
 
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("org.postgresql:postgresql")
